7a729c59edfe036b9e8dcc2670155af5f18f73c4
Sonja Lechner <blaquekaktus@gmail.com>
2023-02-01 11:30:36 +0100

    Initial commit

---
+++ src/main/java/com/itkolleg/kassenprogamm/KassenProgammApplication.java
@@ -0,0 +1,13 @@
+package com.itkolleg.kassenprogamm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KassenProgammApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KassenProgammApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itkolleg/kassenprogamm/KassenProgammApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkolleg.kassenprogamm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KassenProgammApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
